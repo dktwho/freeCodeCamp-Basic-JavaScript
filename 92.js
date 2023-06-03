691881eb240e254f076153e7a9456d37b74c94fe
Alexander Korobov <dktwho@mail.ru>
2023-06-03 10:04:08 +0300

    BasicJS 92

---
+++ 92.js
@@ -0,0 +1,15 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic.push({'artist': 'Bob', 'title': 'Bob song', 'release_year': 1984, 'formats': ['CD', '8t', 'LP']})
