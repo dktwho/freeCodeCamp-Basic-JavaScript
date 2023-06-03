fcf75b520404a63c759f58b37dfe0d75a7d310a3
Alexander Korobov <dktwho@mail.ru>
2023-06-03 09:07:45 +0300

    BasicJS 70

---
+++ 70.js
@@ -0,0 +1,12 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  } else   return "Inside";
+
+  // Only change code above this line
+
+}
+
+testLogicalOr(15);
